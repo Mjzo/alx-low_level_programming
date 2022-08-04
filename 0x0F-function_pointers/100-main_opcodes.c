9cc542ec75423087e4530f9ae1638efcf10a0e0e
mjzo <josephmusembimukula@gmail.com>
2022-08-03 22:17:48 -0700

    program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
+#include <stdio.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- * Return: ...
+ * main - prints opcodes of a given mahine
+ * @argc: number of arguments.
+ * @argv: argument vector
+ *
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int i, size;
 
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
-
+	size = atoi(argv[1]);
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < size - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
+
